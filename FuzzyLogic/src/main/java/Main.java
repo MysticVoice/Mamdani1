c167c5c61c9a2a40f797c71694302b4f466d7bef
MysticVoice <fredrimv@hotmail.com>
2016-10-06 15:47:50 +0200

    update

--- FuzzyLogic/src/main/java/Main.java
+++ FuzzyLogic/src/main/java/Main.java
@@ -13,15 +13,42 @@ public class Main {
     
     public static void main(String args[])
     {
-        FuzzyMem fuzzy = new FuzzyMem("test");
-        float[] arguments = new float[4];
-        float input = (float)1.5;
-        arguments[0] = 0;
-        arguments[1] = 1;
-        arguments[2] = 2;
-        arguments[3] = 3;
-        fuzzy.addFuzzifier("", "trapmf", arguments);
-        System.out.println("Result was: " + fuzzy.fuzzify(input)[0]);
-        
+//        FuzzyMem fuzzy = new FuzzyMem("test");
+//        
+//        float[] arguments = new float[4];
+//        float input = (float)2.8;
+//        arguments[0] = 0;
+//        arguments[1] = 1;
+//        arguments[2] = 2;
+//        arguments[3] = 3;
+//        fuzzy.addFuzzifier("Low", "trapmf", arguments);
+//        
+//        float[] arguments2 = new float[4];
+//        arguments2[0] = 2;
+//        arguments2[1] = 3;
+//        arguments2[2] = 4;
+//        arguments2[3] = 5;
+//        fuzzy.addFuzzifier("Medium", "trapmf", arguments2);
+//        
+//        float[] arguments3 = new float[4];
+//        arguments3[0] = 4;
+//        arguments3[1] = 5;
+//        arguments3[2] = 6;
+//        arguments3[3] = 7;
+//        fuzzy.addFuzzifier("High", "trapmf", arguments3);
+//        
+//        float[] arguments4 = new float[4];
+//        arguments4[0] = 6;
+//        arguments4[1] = 7;
+//        arguments4[2] = 8;
+//        arguments4[3] = 9;
+//        fuzzy.addFuzzifier("Very High", "trapmf", arguments4);
+//        
+//        float[] result= fuzzy.fuzzify(input);
+//        System.out.println("Results were: " + result[0]+" , "+result[1]+" , "+result[2] + " , "+result[3]);
+//        
+        FuzzySystem sys = new FuzzySystem("House Evaluation");
+        sys.addInput("Apartment value");
+        sys.addInput("Location");
     }
 }
--- FuzzyLogic/src/main/java/RuleBase.java
+++ FuzzyLogic/src/main/java/RuleBase.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +13,24 @@
  */
 public class RuleBase {
     
+    ArrayList<int[]> rules;
     public RuleBase()
+    {
+        rules = new ArrayList<>();
+    }
+    
+    public void addRule(int[] inputs, int targetOutput)
+    {
+        int[] rule = new int[(inputs.length+1)];
+        for(int i = 0; i <inputs.length;i++)
+        {
+            rule[i] = inputs[i];
+        }
+        rule[inputs.length] = targetOutput;
+        rules.add(rule);
+    }
+    
+    public void useRules(float[] values)        
     {
         
     }
